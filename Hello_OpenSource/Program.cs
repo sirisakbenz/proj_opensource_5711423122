2dfbcf5ee65c38a554842ff0bf0a6789c10a3f44
sirisakbenz <sirisak.sais57@sskru.ac.th>
2016-09-09 14:06:55 +0700

    add project to repository

---
+++ Hello_OpenSource/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+namespace Hello_OpenSource
+{
+    class Hello
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello My Name is sirisak saisorn 5711423122");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
